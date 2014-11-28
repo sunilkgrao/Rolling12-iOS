20688f4437ef996c9c4389b3b4a3feee896fd04b
Sunil Rao <sunilkgrao@gmail.com>
2014-11-28 00:42:58 -0500

    Initial Commit

---
+++ Rolling12-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rolling12-iOS
+//
+//  Created by Sunil Rao on 2014-11-28.
+//  Copyright (c) 2014 Baryon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
